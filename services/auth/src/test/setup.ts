429b78936bb163a041847149dc85e97ccac2edbc
Yanivv77 <Yanivv77@gmail.com>
2022-03-30 21:09:40 +0300

    auth service done

--- services/auth/src/test/setup.ts
+++ services/auth/src/test/setup.ts
@@ -1,11 +1,20 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
+
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
   mongo = await MongoMemoryServer.create();
+
   process.env.JWT_KEY = 'a2sd2fj';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
   const uri = mongo.getUri();
   await mongoose.connect(uri);
 });
@@ -23,4 +32,22 @@ afterAll(async () => {
   await mongo.stop();
   
 });
+
+
  
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+}
